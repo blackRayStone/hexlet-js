8c41736837d118b58b2d1691efc6ba6bd700767e
Oleg Samoilenko <uardan@gmail.com>
2022-11-10 18:46:28 +0300

    update index.js, package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 import _ from "lodash";
-console.log('FINE!');
+console.log("FINE!");
 
 let a = 10;
 let b = 15;
 
 const sum = (a, b) => {
-    return a + b;
-
-}
+  return a + b;
+};
 
 console.log(sum(a, b));
 
 console.log(_.capitalize("hello"));
-console.log(_.last(['one','two']));
+console.log(_.last(["one", "two"]));
